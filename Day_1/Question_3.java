ec86135956bf43c451a13b9cd802ff7cf1bc2181
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-05-28 22:51:49 +0530

    first commit

---
+++ Day_1/Question_3.java
@@ -0,0 +1,15 @@
+package Day_1;
+import java.util.Scanner;
+
+public class Question_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact*i;
+        }
+        System.out.println(fact);
+        sc.close();
+    }
+}
