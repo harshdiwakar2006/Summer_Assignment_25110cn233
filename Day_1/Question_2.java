9ad38d97c938e044dad2610c4a3af95a0e9ac6a1
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-05-28 22:41:23 +0530

    first commit

---
+++ Day_1/Question_2.java
@@ -0,0 +1,14 @@
+package Day_1;
+import java.util.Scanner;
+
+public class Question_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. :");
+        int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++){
+            System.out.println(n+" X "+i+" = "+n*i);
+        }
+        sc.close();
+    }
+}
