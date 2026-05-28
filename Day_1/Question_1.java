755a00958ac8bfdbc8cedb9ec00a368beeb52572
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-05-28 22:10:07 +0530

    first commit

---
+++ Day_1/Question_1.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+import java.util.Scanner;
+public class Question_1 {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no. :");
+        int n = sc.nextInt();
+        int c = 0;
+        for (int i = 1; i <= n; i++){
+            c = c+i;
+        }
+        System.out.println("The sum of first "+n+" is "+c);
+
+    }
+}
