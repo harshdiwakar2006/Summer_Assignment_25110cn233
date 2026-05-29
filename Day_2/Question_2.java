c5a9987bd9ba4926683ed26d16c5023bec130cfd
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-05-29 13:13:55 +0530

    first commit

--- Day_2/Question_2.java
+++ Day_2/Question_2.java
@@ -2,23 +2,17 @@ package Day_2;
 
 import java.util.Scanner;
 
-public class Question_1 {
+public class Question_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter the number : ");
         int n = sc.nextInt();
-
         int d = 0;
-
-        while (n != 0) {
-
-            d = d + (n%10);
-
-            n = n / 10;
+        while(n != 0){
+            d = (n%10) + (d*10);
+            n/=10;
         }
-
-        System.out.println("The sum of digits is = " + d);
-
+        System.out.println("The reverse of the number is " + d);
         sc.close();
     }
 }
