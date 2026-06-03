1190f41c7a124038d2cab25803eea14c5e07db67
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-06-03 13:51:54 +0530

    first commit

---
+++ Day-3/Question_4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Question_4 {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        // Find GCD using Euclidean Algorithm
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+
+        sc.close();
+    }
+}
