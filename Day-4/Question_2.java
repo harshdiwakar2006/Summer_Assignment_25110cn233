9f473e928720de81bb81ef885c858c6c46c3f630
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-06-04 18:05:27 +0530

    first commit

---
+++ Day-4/Question_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Question_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Nth term : ");
+        int n = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int c = 0;
+        for (int i = 1; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        System.out.println("The nth term of the series is : " + c);
+        sc.close();
+    }
+}
