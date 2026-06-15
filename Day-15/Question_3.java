81490c5a315ba9febe2d3ec8e7df3b2b681b70bb
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-06-15 21:28:17 +0530

    first commit

---
+++ Day-15/Question_3.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Question_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Right rotation by 1
+        int last = arr[n - 1];
+
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+
+        System.out.println("Array after right rotation:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+}
