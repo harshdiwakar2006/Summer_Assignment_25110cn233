f2aff998d588ae687105618a3b6336677ce9276a
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-06-21 16:41:11 +0530

    first commit

---
+++ Day-21/Question_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {
+            // End of string reached
+        }
+
+        System.out.println("Length of the string = " + count);
+    }
+}
